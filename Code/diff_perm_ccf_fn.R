59e36b0e434dd37fb66c030898ccc7661207148e
duncanobrien <duncan.a.obrien@gmail.com>
2021-10-11 09:47:01 +0100

    Initial push

---
+++ Code/diff_perm_ccf_fn.R
@@ -0,0 +1,397 @@
+###########################################################################################
+### diff.perm.ccf function ###
+###########################################################################################
+#@ts = vector of functional diversity values
+#@timedat = vector of dates/time index
+#@span = number of time points to cross correlate and/or spatial permute across
+#@iter = number of permutations
+#@comp.ts = vector of values to cross correlate against
+#@comp.ts.timedat = vector of dates/time index for the comparative time series
+#@diff = should both ts and comp.ts be differenced
+#@lag = how many time points should the differencing take place across
+#@pre.diff = is the comp.ts pre differenced?
+#@perm.method = which pseudo-randmomisation technique should be used:
+              # "spatial" (shuffling within temporal span),
+              # "arima" (residuals of ARIMA model),
+              # "red.noise" (red noise process using data mean, variance and autocorrelation coef) 
+              # or "replacement" (sample with replacement)
+#@scale = should both ts and comp.ts be centered to mean 0 and scaled to unit variance
+#@normalise = should both ts and comp.ts be normalised by ranking observations by their distribution
+#@identical.t = for cross correlation, should the permuted correlation coefs be taken from the same optimal 
+              #lag as the observed (identical.t = T) or from the optimal lag of the permuted ts (identical.t = F)
+
+
+diff.perm.ccf <- function(ts, timedat, span = 12*5, iter = 999,
+                          comp.ts,comp.ts.timedat = NULL,diff = T, 
+                          lag=12,pre.diff = F,
+                          perm.method = c("spatial","arima","red.noise","replacement"),
+                          scale = F,normalise = F,identical.t = F){
+  
+  require(fgpt) #spatial permutation
+  require(foreach)
+  require(dplyr)
+  require(forecast)
+  require(zoo)
+  set.seed(123) 
+  
+  ## Necessary custom functions ##
+  comb <- function(...) {
+    mapply('rbind', ..., SIMPLIFY=FALSE)
+  } #combination function for 'foreach'
+  
+  red.noise.ts <- function(ts,lag,length){
+    x  <- rep(NA, length)
+    ts <- rnorm(length,mean = mean(ts,na.rm=T),sd=sd(ts,na.rm=T))
+    x[1] <- ts[1]
+    for(i in seq(from = 2, to = length, by =1)){
+      x[i] <- lag*x[i-1] + ((1-lag^2)^0.5)*ts[i]
+    }
+    return(x) #create rednoise process from a time series' mean, variance and autocorrelation coef
+  }
+  
+  
+  ## Create and register cluster for parallelisation
+  cl <- parallel::makeCluster(3L)
+  doParallel::registerDoParallel(cl)
+  
+  perm.meth <- match.arg(perm.method)
+  
+  if(any(is.na(ts))){
+    print("Warning: ts has missing values. NAs have been interpolated")
+    ts <- zoo::na.approx(ts,na.rm=F)
+  }
+  if(any(is.na(comp.ts))){
+    print("Warning: comp.ts has missing values. NAs have been interpolated")
+    comp.ts <- zoo::na.approx(comp.ts,na.rm=F)
+  }
+  
+  if(isTRUE(diff)){
+    if(isFALSE(pre.diff)){
+      ts.diff <- diff(ts, lag = lag)
+      comp.diff <- diff(comp.ts, lag = lag)
+      
+    }else{
+      ts.diff <- diff(ts, lag = lag)
+      comp.diff <- comp.ts
+    }
+  }else{
+    ts.diff <- ts
+    comp.diff <- comp.ts
+  }
+  ##normalise derivatives via quantiles
+  if(isTRUE(normalise)){
+    ts.diff.ecdf <- ecdf(ts.diff)
+    ts.diff <- ts.diff.ecdf(ts.diff)
+    comp.diff.ecdf <- ecdf(comp.diff)
+    comp.diff <- comp.diff.ecdf(comp.diff)
+  }else{
+    ts.diff <- ts.diff
+    comp.diff <- comp.diff
+  }
+  
+  
+  ##scale derivatives to mean zero and equal SD
+  if(isTRUE(scale)){
+    ts.diff <- c(scale(ts.diff))
+    comp.diff <- c(scale(comp.diff))
+  }else{
+    ts.diff <- ts.diff
+    comp.diff <- comp.diff
+  }
+  
+  #match timedat if not 1:1 (& drop comp.ts dates X in FD data)
+  if(!is.null(comp.ts.timedat)){
+    #ts.diff <- na.omit(ts.diff[(comp.ts.timedat-lag)])
+    ts.diff <- na.omit(ts.diff[(comp.ts.timedat)])
+  }
+  
+  #calculate observed correlation
+  ccf.tmp <- ccf(ts.diff,comp.diff,lag.max = span,plot = F)
+  ccf.tmp <- data.frame("lag" = ccf.tmp$lag,"acf" = ccf.tmp$acf)
+  
+  
+  ccf.obs <- data.frame("tmin" = ccf.tmp$lag[ccf.tmp$acf == min(ccf.tmp$acf,na.rm=TRUE)])
+  ccf.obs$rmin <-  ccf.tmp$acf[ccf.tmp$lag == ccf.obs$tmin]
+  ccf.obs$tmax <- ccf.tmp$lag[ccf.tmp$acf == max(ccf.tmp$acf,na.rm=TRUE)]
+  ccf.obs$rmax <-  ccf.tmp$acf[ccf.tmp$lag == ccf.obs$tmax]
+  ccf.obs$r0 <- ccf.tmp$acf[ccf.tmp$lag == 0]
+  ccf.obs$t.absmax <- ccf.tmp$lag[abs(ccf.tmp$acf) == max(abs(ccf.tmp$acf),na.rm=TRUE)]
+  ccf.obs$abs.rmax <- ccf.tmp$acf[ccf.tmp$lag == ccf.obs$t.absmax]
+  
+  ## spatial autocorrelation based permutation
+  if(perm.meth == "spatial"){
+    ## Create permutation order
+    xy <- cbind(timedat,timedat) #temporal 'grid references' for spatial permutation
+    
+    perm.df <- fgpt::fgperm(xy,marks=ts, scale=span,  iter=iter, ratio=1, FUN=fgpt::fyshuffle, 
+                            bootstrap = F, add.obs=F, as.matrix=F) #create semi-random order of indices
+    
+    ##loop through each permutation to fit GAMM and extract derivatives
+    tmp <- foreach::foreach(i = c(1:iter),.combine = "comb",.multicombine = T, .packages = c("mgcv","gratia","dplyr")) %dopar%{
+      
+      if(isTRUE(diff)){
+        ts.diff.perm <- diff(ts[perm.df[[i]]], lag = lag)
+      }else{
+        ts.diff.perm <- ts[perm.df[[i]]]
+      }
+      
+      ##normalise derivatives via quantiles
+      if(isTRUE(normalise)){
+        ts.diff.perm.ecdf <- ecdf(ts.diff.perm)
+        ts.diff.perm <- ts.diff.perm.ecdf(ts.diff.perm)
+      }else{
+        ts.diff.perm <- ts.diff.perm
+      }
+      ##scale derivatives to mean zero and equal SD
+      if(isTRUE(scale)){
+        ts.diff.perm <- c(scale(ts.diff.perm))
+      }else{
+        ts.diff.perm <- ts.diff.perm
+      }
+      
+      
+      #match timedat if not 1:1
+      if(!is.null(comp.ts.timedat)){
+        ts.diff.perm <- na.omit(ts.diff.perm[(comp.ts.timedat)])
+      }
+      
+      #calculate observed correlation
+      ccf.perm.tmp <- ccf(ts.diff.perm,comp.diff,lag.max = span,plot = F)
+      ccf.perm.tmp <- data.frame("lag" = ccf.perm.tmp$lag,"acf" = ccf.perm.tmp$acf)
+      
+      
+      if(isTRUE(identical.t)){
+        ccf.perm.obs <- data.frame("tmin" =ccf.obs$tmin) #match observed tmin/tmax for comparison
+        ccf.perm.obs$rmin <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmin]
+        ccf.perm.obs$tmax <- ccf.obs$tmax
+        ccf.perm.obs$rmax <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmax]
+        ccf.perm.obs$r0 <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == 0]
+        ccf.perm.obs$t.absmax <- ccf.obs$t.absmax
+        ccf.perm.obs$abs.rmax <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$t.absmax]
+      }else{
+        ccf.perm.obs <- data.frame("tmin" = ccf.perm.tmp$lag[ccf.perm.tmp$acf == min(ccf.perm.tmp$acf,na.rm=TRUE)])
+        ccf.perm.obs$rmin <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmin]
+        ccf.perm.obs$tmax <- ccf.perm.tmp$lag[ccf.perm.tmp$acf == max(ccf.perm.tmp$acf,na.rm=TRUE)]
+        ccf.perm.obs$rmax <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmax]
+        ccf.perm.obs$r0 <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == 0]
+        ccf.perm.obs$t.absmax <- ccf.perm.tmp$lag[abs(ccf.perm.tmp$acf) == max(abs(ccf.perm.tmp$acf),na.rm=TRUE)]
+        ccf.perm.obs$abs.rmax <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$t.absmax]
+      }
+      
+      return(ccf.perm.obs)
+    }
+  }
+  
+  ## ARIMA based permutation
+  if(perm.meth == "arima"){
+    ## Create permutation order
+    fit <- forecast::auto.arima(stats::as.ts(ts),allowdrift =TRUE,seasonal = T)
+    perm.df <- data.frame(date = timedat[,1],
+                          pred = fitted(fit),
+                          upr = fitted(fit) + 1.96*sqrt(fit$sigma2),
+                          lwr = fitted(fit) - 1.96*sqrt(fit$sigma2))
+    for (perm in c(1:iter)) { # for each permutation randomly sample from confidence interval at each time point
+      new_col_name <- paste0("perm_", perm) # new column and colname for each permutation
+      perm.df <- perm.df %>% 
+        rowwise()%>%
+        mutate(!!sym(new_col_name) := sample(x = seq(from = lwr, to = upr,by = 0.0001),size = 1))%>%
+        ungroup()
+    }
+    tmp <- foreach::foreach(i = 1:iter,.combine = "comb",.multicombine = T, .packages = c("dplyr")) %dopar%{     
+      if(isTRUE(monthly)){ # first difference (seasonal)
+        ts.diff.perm <- as.numeric(base::diff(as.ts(perm.df[,paste("perm",i,sep = "_")]), lag = 12)) #as.ts required due to finicky tibble interactions with diff
+      }else{ #first difference (yearly)
+        ts.diff.perm <- as.numeric(diff(as.ts(perm.df[,paste("perm",i,sep = "_")]), lag = 1))
+      }
+      
+      ##normalise derivatives via quantiles
+      if(isTRUE(normalise)){
+        ts.diff.perm.ecdf <- ecdf(ts.diff.perm)
+        ts.diff.perm <- ts.diff.perm.ecdf(ts.diff.perm)
+      }else{
+        ts.diff.perm <- ts.diff.perm
+      }
+      ##scale derivatives to mean zero and equal SD
+      if(isTRUE(scale)){
+        ts.diff.perm <- c(scale(ts.diff.perm))
+      }else{
+        ts.diff.perm <- ts.diff.perm
+      }
+      
+      #calculate observed correlation
+      if(monthly == T){
+        ccf.perm.tmp <- ccf(ts.diff.perm,comp.diff,lag.max = 12*5,plot = F)
+        ccf.perm.tmp <- data.frame("lag" = ccf.perm.tmp$lag,"acf" = ccf.perm.tmp$acf)
+        
+      }else{
+        ccf.perm.tmp <- ccf(ts.diff.perm,comp.diff,lag.max = 5,plot = F)
+        ccf.perm.tmp <- data.frame("lag" = ccf.perm.tmp$lag,"acf" = ccf.perm.tmp$acf)
+      }
+      
+      ccf.perm.obs <- data.frame("tmin" =ccf.obs$tmin) #match observed tmin/tmax for comparison
+      ccf.perm.obs$rmin <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmin]
+      ccf.perm.obs$tmax <- ccf.obs$tmax
+      ccf.perm.obs$rmax <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmax]
+      ccf.perm.obs$r0 <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == 0]
+      ccf.perm.obs$t.absmax <- ccf.obs$t.absmax
+      ccf.perm.obs$abs.rmax <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$t.absmax]
+      
+      return(ccf.perm.obs)
+    }
+  }
+  
+  ## rednoise based permutation
+  if(perm.meth == "red.noise"){
+    ## Create permutation order
+    # fit ar1 model to ts and extract autocorrelation coefficient
+    if(span>=12){
+      d1.ar1 <- stats::arima(stats::as.ts(ts), order = c(1, 0, 0),
+                             seasonal = list(order = c(1, 0, 0), period = 12),
+                             optim.control = list(maxit = 1000),method="ML")$coef[1]
+    }else{
+      d1.ar1 <- stats::arima(stats::as.ts(ts), order = c(1, 0, 0),optim.control = list(maxit = 1000),method="ML")$coef[1]
+    }
+    
+    perm.df <- matrix(NA,nrow=length(timedat),ncol=iter)
+    
+    for (r in seq_len(iter)) {
+      perm.df[,r] <- red.noise.ts(ts,lag=d1.ar1,length=length(timedat)) #permuted autocorrelated surrogates
+    }  
+    
+    ##loop through each permutation to fit GAMM and extract derivatives
+    tmp <- foreach::foreach(i = 1:iter,.combine = "comb",.multicombine = T, .packages = c("dplyr")) %dopar%{
+      if(isTRUE(diff)){
+        ts.diff.perm <- diff(perm.df[,i], lag = lag)
+      }else{
+        ts.diff.perm <- perm.df[,i]
+      }
+      ##normalise derivatives via quantiles
+      if(isTRUE(normalise)){
+        ts.diff.perm.ecdf <- ecdf(ts.diff.perm)
+        ts.diff.perm <- ts.diff.perm.ecdf(ts.diff.perm)
+      }else{
+        ts.diff.perm <- ts.diff.perm
+      }
+      ##scale derivatives to mean zero and equal SD
+      if(isTRUE(scale)){
+        ts.diff.perm <- c(scale(ts.diff.perm))
+      }else{
+        ts.diff.perm <- ts.diff.perm
+      }
+      
+      #match timedat if not 1:1
+      if(!is.null(comp.ts.timedat)){
+        ts.diff.perm <- na.omit(ts.diff.perm[(comp.ts.timedat)])
+      }
+      
+      #calculate observed correlation
+      ccf.perm.tmp <- ccf(ts.diff.perm,comp.diff,lag.max = span,plot = F)
+      ccf.perm.tmp <- data.frame("lag" = ccf.perm.tmp$lag,"acf" = ccf.perm.tmp$acf)
+      
+      if(isTRUE(identical.t)){
+        ccf.perm.obs <- data.frame("tmin" =ccf.obs$tmin) #match observed tmin/tmax for comparison
+        ccf.perm.obs$rmin <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmin]
+        ccf.perm.obs$tmax <- ccf.obs$tmax
+        ccf.perm.obs$rmax <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmax]
+        ccf.perm.obs$r0 <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == 0]
+        ccf.perm.obs$t.absmax <- ccf.obs$t.absmax
+        ccf.perm.obs$abs.rmax <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$t.absmax]
+      }else{
+        ccf.perm.obs <- data.frame("tmin" = ccf.perm.tmp$lag[ccf.perm.tmp$acf == min(ccf.perm.tmp$acf,na.rm=TRUE)])
+        ccf.perm.obs$rmin <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmin]
+        ccf.perm.obs$tmax <- ccf.perm.tmp$lag[ccf.perm.tmp$acf == max(ccf.perm.tmp$acf,na.rm=TRUE)]
+        ccf.perm.obs$rmax <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmax]
+        ccf.perm.obs$r0 <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == 0]
+        ccf.perm.obs$t.absmax <- ccf.perm.tmp$lag[abs(ccf.perm.tmp$acf) == max(abs(ccf.perm.tmp$acf),na.rm=TRUE)]
+        ccf.perm.obs$abs.rmax <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$t.absmax]
+      }
+      
+      return(ccf.perm.obs)
+    }
+  }
+  
+  ## resample with replacement
+  if(perm.meth == "replacement"){
+    ## Create permutation order
+    perm.df <- data.frame(date = timedat,
+                          ts=ts)
+    for (perm in c(1:iter)) { # for each permutation randomly sample from ts with replacement at each time point
+      new_col_name <- paste0("perm_", perm) # new column and colname for each permutation
+      perm.df <- perm.df %>% 
+        mutate(!!sym(new_col_name) :=sample(x = ts,replace = TRUE,size = length(ts)))
+    }
+    tmp <- foreach::foreach(i = 1:iter,.combine = "comb",.multicombine = T, .packages = c("dplyr")) %dopar%{     
+      
+      if(isTRUE(diff)){ 
+        ts.diff.perm <- diff(perm.df[,paste("perm",i,sep = "_")], lag = lag)
+      }else{ 
+        ts.diff.perm <- perm.df[,paste("perm",i,sep = "_")]
+      }
+      
+      ##normalise derivatives via quantiles
+      if(isTRUE(normalise)){
+        ts.diff.perm.ecdf <- ecdf(ts.diff.perm)
+        ts.diff.perm <- ts.diff.perm.ecdf(ts.diff.perm)
+      }else{
+        ts.diff.perm <- ts.diff.perm
+      }
+      ##scale derivatives to mean zero and equal SD
+      if(isTRUE(scale)){
+        ts.diff.perm <- c(scale(ts.diff.perm))
+      }else{
+        ts.diff.perm <- ts.diff.perm
+      }
+      
+      
+      #match timedat if not 1:1
+      if(!is.null(comp.ts.timedat)){
+        ts.diff.perm <- na.omit(ts.diff.perm[(comp.ts.timedat)])
+      }
+      
+      #calculate observed correlation
+      ccf.perm.tmp <- ccf(ts.diff.perm,comp.diff,lag.max = span,plot = F)
+      ccf.perm.tmp <- data.frame("lag" = ccf.perm.tmp$lag,"acf" = ccf.perm.tmp$acf)
+      
+      if(isTRUE(identical.t)){
+        ccf.perm.obs <- data.frame("tmin" =ccf.obs$tmin) #match observed tmin/tmax for comparison
+        ccf.perm.obs$rmin <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmin]
+        ccf.perm.obs$tmax <- ccf.obs$tmax
+        ccf.perm.obs$rmax <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmax]
+        ccf.perm.obs$r0 <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == 0]
+        ccf.perm.obs$t.absmax <- ccf.obs$t.absmax
+        ccf.perm.obs$abs.rmax <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$t.absmax]
+      }else{
+        ccf.perm.obs <- data.frame("tmin" = ccf.perm.tmp$lag[ccf.perm.tmp$acf == min(ccf.perm.tmp$acf,na.rm=TRUE)])
+        ccf.perm.obs$rmin <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmin]
+        ccf.perm.obs$tmax <- ccf.perm.tmp$lag[ccf.perm.tmp$acf == max(ccf.perm.tmp$acf,na.rm=TRUE)]
+        ccf.perm.obs$rmax <-  ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$tmax]
+        ccf.perm.obs$r0 <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == 0]
+        ccf.perm.obs$t.absmax <- ccf.perm.tmp$lag[abs(ccf.perm.tmp$acf) == max(abs(ccf.perm.tmp$acf),na.rm=TRUE)]
+        ccf.perm.obs$abs.rmax <- ccf.perm.tmp$acf[ccf.perm.tmp$lag == ccf.perm.obs$t.absmax]
+      }
+      
+      
+      return(ccf.perm.obs)
+    }
+  }
+  parallel::stopCluster(cl)
+  
+  out <- matrix(NA,nrow = 7,ncol=5) #using permuted metrics, estimate permuted median and quantile of observed data
+  
+  out[1,] <- c("rmin.ccf",ecdf(tmp$rmin)(ccf.obs$rmin),ccf.obs$rmin,median(tmp$rmin),ccf.obs$rmin - median(tmp$rmin))
+  out[2,] <- c("t.rmin.ccf",ecdf(tmp$tmin)(ccf.obs$tmin),ccf.obs$tmin,median(tmp$tmin),ccf.obs$tmin - median(tmp$tmin))
+  out[3,] <- c("rmax.ccf",ecdf(tmp$rmax)(ccf.obs$rmax),ccf.obs$rmax,median(tmp$rmax),ccf.obs$rmax - median(tmp$rmax))
+  out[4,] <- c("t.rmax.ccf",ecdf(tmp$tmax)(ccf.obs$tmax),ccf.obs$tmax,median(tmp$tmax),ccf.obs$tmax - median(tmp$tmax))
+  out[5,] <- c("r0.ccf",ecdf(tmp$r0)(ccf.obs$r0),ccf.obs$r0,median(tmp$r0),ccf.obs$r0 - median(tmp$r0))
+  out[6,] <- c("absmax.ccf",ecdf(tmp$abs.rmax)(ccf.obs$abs.rmax),ccf.obs$abs.rmax,median(tmp$abs.rmax),ccf.obs$abs.rmax - median(tmp$abs.rmax))
+  out[7,] <- c("t.absmax.ccf",ecdf(tmp$t.absmax)(ccf.obs$t.absmax),ccf.obs$t.absmax,median(tmp$t.absmax),ccf.obs$t.absmax - median(tmp$t.absmax))
+  
+  
+  
+  out <- data.frame(out) %>% 
+    stats::setNames(c("measure","quantile","obs.value","median.perm.value","obs.difference")) %>%
+    mutate(across(quantile:obs.difference, ~as.numeric(as.character(.)))) #ensure numeric data not character
+  
+  out.ls <- list("perm.dens" = tmp, "summary" = out)
+  return(out.ls)
+}
---
+++ Code/tidy_FD_fn.R
@@ -0,0 +1,193 @@
+###########################################################################################
+### TidyFD function ###
+###########################################################################################
+#@speciesdat = matrix of species abundances, names across columns, time across rows
+#@traitdat = matrix of species traits, names across rows, traits across columns
+#@traittype = fuzzy/ordinal
+#@trophic.lvl = phyto(plankton), zoo(plankton) or fish. For classification
+#@method = which functional diversity metric to be calculated. Multiple can be provided
+#@correction = to correct for non-Euclidean distances during construction of trait space
+#@ndim = maximum number of dimensions for trait space
+
+tidyFD <- function(speciesdat, traitdat, trophic.lvl = c("phyto","zoo","fish"), traittype = c("fuzzy","ordinal"),
+                   method = c("MNND","Rao","FDis","FRic","FEve","FDiv","SRic","SDiv","RARDis","RARScr"),
+                   correction = c("sqrt", "cailliez", "lingoes", "none"), ndim = 10){
+  require(tibble)
+  require(dplyr)
+  require(FD)
+  require(picante)
+  require(ade4)
+  require(vegan)
+  require(gawdis)
+  require(funrar)
+  set.seed(1234)
+  
+source("/Code/melodic_fn.R")
+  
+  #select trait data type. Fuzzy used in this work
+  if(traittype == "ordinal"){
+    #ensure matched species timeseries and trait data
+    species.data <- speciesdat[,colSums(speciesdat != 0) > 0] #drop any full zero timeseries
+    traits.data <- traitdat[rowSums(is.na(traitdat)) != (ncol(traitdat)-1),] #drop any full NA species trait vectors                                                                                                      
+    
+    a <- colnames(species.data)
+    b <- traits.data$Species
+    c <- intersect(a,b) #match species present in both datasets
+    
+    species.data<- species.data[, c]
+    traits.data <- traits.data[traits.data$Species %in% c, ]
+    
+    if(length(which(trophic.lvl == "phyto"))==1){
+      traits.data <- traits.data %>%
+        mutate_at(c("Cell length", "Cell SA","Cell biovol",
+                    "Flagellated", "Colonial","Filamentous", "Heterotrophic","Mixotrophic",
+                    "Nitrogen"), as.numeric) %>% 
+        mutate_at(c("Geometrical shape","Morphoclass"),as.factor) %>%
+        column_to_rownames(var = "Species")
+      
+      traits.data.in <- as.matrix(gowdis(traits.data))
+    }
+    
+    
+    if(length(which(trophic.lvl == "zoo"))==1){
+      
+      traits.data <- traits.data %>% 
+        mutate_at(c("Trophic group", "Feeding mode","Reproductive mechanism"), as.factor) %>% 
+        mutate_at(c("Mean length μm"),as.numeric) %>%
+        #traits.data$`Feeding mode`<- factor(traits.data$`Feeding mode`, levels = c("Cruise","Current","Active ambush"))
+        ##traits.data$`Trophic group`<- factor(traits.data$`Trophic group`, levels = c("Mi","Ma","P"), ordered = T)
+        #traits.data$`Mean length μm`<- as.numeric(traits.data$`Mean length μm`)
+        #traits.data$`Reproductive mechanism`<- factor(traits.data$`Reproductive mechanism`, levels = c("sac","cyc parth","noncyc parth"))
+        column_to_rownames( var = "Species")
+      
+      traits.data.in <- as.matrix(gowdis(traits.data))  
+    }
+    
+    if(length(which(trophic.lvl == "fish"))==1){
+      
+      traits.data.in <- gowdis(traits.data)
+    }
+  }
+  
+  if(traittype=="fuzzy"){
+    species.data <- speciesdat[,colSums(speciesdat != 0) > 0] #drop any full zero timeseries
+    traits.data <- traitdat[rowSums(is.na(traitdat)) != (ncol(traitdat)-1),] #drop any full NA species trait vectors                                                                                                      
+    
+    a <- colnames(species.data)
+    b <- traits.data$Name
+    int <- intersect(a,b) #match species present in both datasets
+    
+    species.data <- species.data[, int]
+    traits.data <- traits.data[traits.data$Name %in% int, ]
+    
+    if(length(which(trophic.lvl == "phyto"))==1){
+      #fuz.trait <-prep.fuzzy.var(df = traits.data[,-c(1:3)], #df = data for  trait (s)
+      #col.blocks = c(4,4,4,4,2,3,3), #col.blocks = num categories per trait
+      #row.w = rep(1, nrow(traits.data[]))) #row.w = weight of each category
+      
+      #fuz.lgth <- prep.fuzzy.var(traits.data[,4:7], #df = data for one trait i.e. cell length
+      #col.blocks = 4, #col.blocks = num categories for trait
+      #row.w = rep(1, nrow(traits.data[,4:7]))) #row.w = weight of each species
+      
+      #fuz.sa <- prep.fuzzy.var(traits.data[,8:11], col.blocks = 4,row.w = rep(1, nrow(traits.data[,8:11])))
+      
+      #fuz.vol <- prep.fuzzy.var(traits.data[,12:15],col.blocks = 4,row.w = rep(1, nrow(traits.data[,12:15])))
+      
+      #fuz.c.ratio <- prep.fuzzy.var(traits.data[,16:19],col.blocks = 4,row.w = rep(1, nrow(traits.data[,16:19])))
+      
+      #fuz.col <- prep.fuzzy.var(traits.data[,20:21],col.blocks = 2,row.w = rep(1, nrow(traits.data[,20:21])))
+      
+      #fuz.mob <- prep.fuzzy.var(traits.data[,22:24],col.blocks = 3,row.w = rep(1, nrow(traits.data[,22:24])))
+      
+      #fuz.troph <- prep.fuzzy.var(traits.data[,25:27],col.blocks = 3,row.w = rep(1, nrow(traits.data[,25:27])))
+      
+      #fuz.n2 <- prep.fuzzy.var(traits.data[,28:29],col.blocks = 2,row.w = rep(1, nrow(traits.data[,28:29])))
+      
+      #fuz.sil <- prep.fuzzy.var(traits.data[,30:31],col.blocks = 2,row.w = rep(1, nrow(traits.data[,30:31])))
+      
+      #ktab <- ktab.list.df(list(fuz.lgth,fuz.sa,fuz.vol,fuz.c.ratio,fuz.col,fuz.mob,fuz.troph,fuz.n2,fuz.sil))
+      #ktab<- ktab.list.df(list(fuz.trait))
+      #traits.data.in <- dist.ktab(ktab,type = c(rep("F",times=9)),scann = F)
+    
+      #each group represents similar traits e.g. size is represented by length/SA/vol
+      #Can also be used for fuzzy data where a group are the categories for a single trait
+      traits.data.in<-gawdis::gawdis(traits.data[,4:19], w.type="equal", 
+                                     groups =c(1,1,1,1,2,2,2,2,2,3,4,4,5,6,7,8), fuzzy=c(1,2,4)) #estimate gawdis dissimilarity for fuzzy data
+      attr(traits.data.in,"Labels") <- int #label rows and columns with species names    
+    }
+    
+    if(length(which(trophic.lvl == "zoo"))==1){
+      traits.data.in<-gawdis::gawdis(traits.data[,5:15], w.type="equal", 
+                                     groups =c(1,1,1,1,2,2,2,2,2,3,4), fuzzy=c(1,2))
+      attr(traits.data.in,"Labels") <- int    
+    }
+  }
+  
+  cor<-match.arg(correction, choices=c("sqrt", "cailliez", "lingoes", "none"), several.ok=F)
+  func.tmp <- match.fun(cor) #convert string to function from FD package
+  meth <- match.arg(method , choices= c("MNND","Rao","FDis","FDiv","FRic","FEve","SRic","SDiv","RARDis","RARScr"), several.ok=T)
+  
+  if(length(which(meth == "MNND"))==1){
+    MNNDout <- picante::mntd(samp = as.matrix(species.data), dis = as.matrix(func.tmp(traits.data.in)), abundance.weighted = T)
+    
+  }
+  
+  if(length(which(meth == "Rao"))==1){
+    RAOout <- melodic(samp = as.matrix(species.data), dis = as.matrix(func.tmp(traits.data.in)),type="abundance")$abundance$rao
+    
+  }
+  
+  if(length(which(grepl("^[RAR]", meth)==1))){
+    rel<- funrar::make_relative(as.matrix(species.data))
+    funrar <- funrar::funrar(as.matrix(rel),as.matrix(traits.data.in),rel_abund = T) 
+    #can't apply correction due to incomaptibility with funrar's calc
+    
+    if(length(which(meth == "RARDis"))==1){
+      RARDISout <- rowMeans(funrar$Di,na.rm=T)
+    }
+    if(length(which(meth == "RARScr"))==1){
+      funrar$Si[is.na(funrar$Si)] <- 1 #if Si is close to 1, the species has relatively low abundances. Therefore 0 abundance would correspond to Si = 1 
+      RARSCRout <- rowMeans(funrar$Si,na.rm=T)
+    }
+  }
+  
+  if(length(which(grepl("^[F]", meth)==1))){ #if meth starts with the letter 'F'
+    tmp.FD <- FD::dbFD(x = traits.data.in,a = as.matrix(species.data), corr = paste(cor), 
+                       calc.CWM = F, calc.FRic = T,calc.FDiv = T,m=ndim) #for speed, create single dbFD object
+    
+    if(length(which(meth == "FDis"))==1){
+      FDISout <- tmp.FD$FDis
+    }
+    if(length(which(meth == "FRic"))==1){
+      FRICout <- tmp.FD$FRic
+    }
+    if(length(which(meth == "FEve"))==1){
+      FEVEout <-tmp.FD$FEve
+    }
+    if(length(which(meth == "FDiv"))==1){
+      FDIVout <-tmp.FD$FDiv
+    }
+  }
+  
+  if(length(which(meth == "SRic"))==1){
+    SRICout <- vegan::specnumber(x = as.matrix(species.data),MARGIN = 1)
+  }
+  
+  if(length(which(meth == "SDiv"))==1){
+    SDIVout <- vegan::diversity(x = as.matrix(species.data),index = "shannon",MARGIN = 1,)
+  }
+  
+  out <- data.frame("MNND" = if(length(which(meth=="MNND"))==1){"MNND"=MNNDout}else{NA},
+                    "Rao" = if(length(which(meth=="Rao"))==1){"Rao"=RAOout}else{NA},
+                    "FRic" = if(length(which(meth=="FRic"))==1){"FRic"=FRICout}else{NA},
+                    "FEve" = if(length(which(meth=="FEve"))==1){"FEve"=FEVEout}else{NA},
+                    "FDiv" = if(length(which(meth=="FDiv"))==1){"FDiv"=FDIVout}else{NA},
+                    "FDis" = if(length(which(meth=="FDis"))==1){"FDis"=FDISout}else{NA},
+                    "SRic" = if(length(which(meth=="SRic"))==1){"SRic"=SRICout}else{NA},
+                    "SDiv" = if(length(which(meth=="SDiv"))==1){"SDiv"=SDIVout}else{NA},
+                    "RARDis" = if(length(which(meth=="RARDis"))==1){"RARDis"=RARDISout}else{NA},
+                    "RARScr" = if(length(which(meth=="RARScr"))==1){"RARScr"=RARSCRout}else{NA}
+  )
+  return(out[,meth])
+  
+}
