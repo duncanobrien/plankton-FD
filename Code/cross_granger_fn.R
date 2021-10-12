8932bd53120907d1f10b626f77dbe44b5cb1e6d9
duncanobrien <duncan.a.obrien@gmail.com>
2021-10-12 11:27:13 +0100

    Initial push

---
+++ Code/cross_granger_fn.R
@@ -0,0 +1,46 @@
+###########################################################################################
+### cross.granger function ###
+###########################################################################################
+# @ts = hypothesised causative variable
+# @comp.ts = response variable
+# @span = number of lags to test causality over
+# @method - "var"/"raw". "var" utilises vector autoregression residuals (package::vars)
+            #whereas "raw" utilises the raw variables (package::lmtest)
+
+cross.granger <- function(ts,comp.ts,span,method = c("var","raw")){
+  
+  sub.dat <- data.frame("ts" = ts, "comp.ts" = comp.ts) # construct data frame. Data frame required in case input ts/comp.ts are scaled
+  
+  if(method == "var"){ #utilise vars::VAR and vars::causality
+    gc.df <- data.frame(statistic = c("x_y.F","x_y.P","x_y.sig","x_y.aic","y_x.F","y_x.P","y_x.sig","y_x.aic")) #initiate df
+    
+    for(kk in c(1:span)){
+      gc_col_name <- paste0("lag_", kk) # new column and colname for each lag
+      
+      obsx <- vars::VAR(sub.dat[,c("ts","comp.ts")], p = kk, type = "both",season = 12) # fit VAR model forward
+      obsy<-  vars::VAR(sub.dat[,c("comp.ts","ts")], p = kk, type = "both",season = 12) # fit VAR model reverse
+      gc.obsx <- vars::causality(obsx,cause = "ts")$Granger # perform forward
+      gc.obsy <- vars::causality(obsy,cause = "comp.ts")$Granger # perform in reverse
+
+      gc.out <- rbind(gc.obsx$statistic,gc.obsx$p.value,ifelse(gc.obsx$p.value <= 0.025,"*",""),AIC(obsx),
+                      gc.obsy$statistic,gc.obsy$p.value,ifelse(gc.obsy$p.value <= 0.025,"*",""),AIC(obsy)) #return Wald test F stat, p-value and AIC
+      gc.df <- gc.df %>% 
+        mutate(!!sym(gc_col_name) := gc.out) # bind to returned data frame with labelled column
+    }
+  }else{
+    
+    gc.df <- data.frame(statistic = c("x_y.F","x_y.P","x_y.sig","y_x.F","y_x.P","y_x.sig")) #initiate df
+    
+    for(kk in c(1:span)){
+      gc_col_name <- paste0("lag_", kk) # new column and colname for each lag
+      
+      gc.obsx <- lmtest::grangertest(sub.dat[,"ts"],sub.dat[,"comp.ts"],order=kk) # perform forward
+      gc.obsy <- lmtest::grangertest(sub.dat[,"comp.ts"],sub.dat[,"ts"],order=kk) # perform in reverse
+      gc.out <- rbind(gc.obsx$F[2],gc.obsx$`Pr(>F)`[2],ifelse(gc.obsx$`Pr(>F)`[2] <= 0.025,"*",""),
+                      gc.obsy$F[2],gc.obsy$`Pr(>F)`[2],ifelse(gc.obsy$`Pr(>F)`[2] <= 0.025,"*","")) #return Wald test F stat and p-value
+      gc.df <- gc.df %>% 
+        mutate(!!sym(gc_col_name) := gc.out) #bind to returned df with labelled column
+    }
+  }
+  return(gc.df)
+}
