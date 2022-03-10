ff1aa551474b3107df757c0a688b16a3dc278ed4
duncanobrien <duncan.a.obrien@gmail.com>
2022-03-10 17:08:45 +0000

    improved plot

--- Results/new_plots.R
+++ Results/new_plots.R
@@ -139,11 +139,11 @@ FEVE.ccf <- ggplot(filter(summary.ccf.mth1,FD.metric %in% "FEve" & measure %in% 
   geom_point(position=position_dodge(width=0.75),
              aes(alpha=sig,group=measure,shape = system),size=2,col="black",fill="black",)+
   geom_point(position=position_dodge(width=0.75),
-             aes(fill=NULL,group=measure,shape = system),size=2,,col="black")+
+             aes(fill=NULL,group=measure,shape = system),size=2,col="black")+
   #facet_grid(troph~state.metric)+
   
   ggh4x::facet_nested(~state.metric+troph,scales = "free",
-                      labeller = label_value,space="free_x" )+
+                      labeller = label_value,space="free_x")+
   scale_shape_manual(values = c(21,22,24,25,23),name = "Lake")+
   scale_colour_manual(values=c("#5F4B8BFF","#E69A8DFF"),name = "Lag",labels = c("Lagx","Lag0")) + 
   scale_fill_manual(values=c("#5F4B8BFF","#E69A8DFF"),name = "Lag",labels = c("Lagx","Lag0"))+
@@ -175,11 +175,17 @@ FRIC.ccf <- ggplot(filter(summary.ccf.mth1,FD.metric %in% "FRic" & measure %in% 
   geom_point(position=position_dodge(width=0.75),
              aes(alpha=sig,group=measure,shape = system),size=2,col="black",fill="black",)+
   geom_point(position=position_dodge(width=0.75),
-             aes(fill=NULL,group=measure,shape = system),size=2,,col="black")+
+             aes(fill=NULL,group=measure,shape = system),size=2,col="black")+
   #facet_grid(troph~state.metric)+
-  
+  # 
+  # ggh4x::facet_nested(~state.metric+troph,scales = "free",
+  #                     labeller = label_value,space="free_x",switch="x",
+  #                     strip = ggh4x::strip_nested(size="constant",bleed=T, 
+  #             background_x =elem_list_rect(fill = c(rep("white",5),rep("#999999",10))),
+  #                                                 by_layer_x = F))+
+  # 
   ggh4x::facet_nested(~state.metric+troph,scales = "free",
-                      labeller = label_value,space="free_x",switch="x")+
+                      labeller = label_value,space="free_x")+
   scale_shape_manual(values = c(21,22,24,25,23),name = "Lake")+
   scale_colour_manual(values=c("#5F4B8BFF","#E69A8DFF"),name = "Lag",labels = c("Lagx","Lag0")) + 
   scale_fill_manual(values=c("#5F4B8BFF","#E69A8DFF"),name = "Lag",labels = c("Lagx","Lag0"))+
@@ -187,7 +193,7 @@ FRIC.ccf <- ggplot(filter(summary.ccf.mth1,FD.metric %in% "FRic" & measure %in% 
                      guide = guide_legend(override.aes = list(fill = c("white","black"),alpha = c(1,1),linetype = c("solid","solid"),shape=c(22,22))))+
   # geom_text(data =count.ccf.r0.dat,aes(x = state.metric, y = ref.y,label = paste("(",NSig,"*",",",NxSig,")",sep = ""),fill=FD.metric,group = FD.metric),
   #           col= "black",size = 3, position = position_dodge(width = 0.9))+
-  scale_x_discrete(labels=c("r0.ccf" = "Lag0", "absmax.ccf" = "Lagx"),limits=c("r0.ccf","absmax.ccf"),position = "top")+
+  scale_x_discrete(labels=c("r0.ccf" = "Lag0", "absmax.ccf" = "Lagx"),limits=c("r0.ccf","absmax.ccf"))+
   scale_y_continuous(limits = c(-0.9,0.9),breaks = seq(-0.75,0.75,0.5))+
   ylab("Cross correlation") + xlab("Time series comparison")+
   guides(size = guide_legend(order = 1), 
@@ -198,9 +204,43 @@ FRIC.ccf <- ggplot(filter(summary.ccf.mth1,FD.metric %in% "FRic" & measure %in% 
   theme_bw() +  theme(plot.margin = unit(c(0,0,0,0), "pt"))
 
 
+lag.scatter.dat <- filter(summary.ccf.mth1,measure %in% "t.absmax.ccf") %>% 
+  mutate(sig = as.factor(filter(summary.ccf.mth1,measure %in% "absmax.ccf")$sig),
+         FD.metric = as.factor(FD.metric))
+
+lagpanel <- ggplot(lag.scatter.dat,
+      aes(x=FD.metric,y=obs.value)) + 
+  geom_hline(yintercept = 0,col="black",alpha = 0.3)+
+  #scale_y_binned(breaks = c(seq(60,24,-12),12,seq(-12,-60,-12)),show.limits = T)+
+    geom_point(position=position_dodge(width=0.75),
+           aes(alpha=sig,group=measure,shape = system),size=2,col="black",fill="black",)+
+  geom_point(position=position_dodge(width=0.75),
+             aes(fill=NULL,group=measure,shape = system),size=2,col="black")+
+  #scale_y_binned(breaks = c(seq(60,24,-12),12,seq(-12,-60,-12)),show.limits = T)+
+  geom_rect(aes(ymax = 12,ymin=-12), xmin = 0,xmax = as.numeric(lag.scatter.dat$FD.metric[[3]])+3,fill="#5F4B8BFF",alpha=0.025)+
+  ggh4x::facet_nested(~state.metric+troph,scales = "free",
+                      labeller = label_value,space="free_x")+
+  scale_shape_manual(values = c(21,22,24,25,23),name = "Lake")+
+  scale_alpha_manual(values=c(0.01,1.0),name = "Significance", labels = c("Not significant","Significant"),
+                     guide = guide_legend(override.aes = list(fill = c("white","black"),alpha = c(1,1),linetype = c("solid","solid"),shape=c(22,22))))+
+  # geom_text(data =count.ccf.r0.dat,aes(x = state.metric, y = ref.y,label = paste("(",NSig,"*",",",NxSig,")",sep = ""),fill=FD.metric,group = FD.metric),
+  #           col= "black",size = 3, position = position_dodge(width = 0.9))+
+  ylab("Strongest lag") + xlab("Functional diversity metric")+
+  guides(size = guide_legend(order = 1), 
+         col = guide_legend(order = 2),
+         fill = guide_legend(order = 2),
+         shape = guide_legend(order = 3))+
+  theme_bw() +  theme(plot.margin = unit(c(0,0,0,0), "pt"))
+
+
+
+
+
+
+
 pdf(file="C:/Users/ul20791/Downloads/sdob_boxplot.pdf",
-    width=13, height = 13)
-FDIS.ccf + FEVE.ccf + FRIC.ccf + plot_layout(nrow = 3,guides = "collect")+
+    width=14, height = 12)
+FDIS.ccf + FEVE.ccf + FRIC.ccf +lagpanel +  plot_layout(nrow = 4,guides = "collect",heights = c(1,1,1,0.9))+
                     plot_annotation(tag_levels = c('a')) & 
                     theme(plot.tag = element_text(face = "bold"))&
   theme(legend.position='right')
